1326effb27b0684d2326cc8873e5b8759cbea8a6
Nitin Jain <official.nitinjain@gmail.com>
2021-03-13 10:50:11 -0500

    added Spring code examples

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,6 +1,8 @@
 package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+ 
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +14,23 @@ public class HomeController {
 		System.out.println("This is home URL");
 		return "home";
 	}
-
+	
+	@RequestMapping("/home1")
+	public String home1() {
+		System.out.println("This is home1 URL");
+		return "home1";
+	}
+	
+	// Passing values to view: 
+	@RequestMapping("/about")
+	public String about(Model model) 
+	
+	{
+		System.out.println("This is about URL");
+		String str = "passing some data" ;
+        model.addAttribute("message", str);
+		   
+		return "about";
+	}
+ 
 }
