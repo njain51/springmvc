960c72f356e60fc18a97b979856175c59b8d88d3
Nitin Jain <official.nitinjain@gmail.com>
2021-03-12 19:09:54 -0500

    first commit

---
+++ src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,16 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	// it will return view name
+	@RequestMapping("/home")
+	public String home() {
+		System.out.println("This is home URL");
+		return "home";
+	}
+
+}
